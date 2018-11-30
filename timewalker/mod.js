39636606831fd069df172c12b987e006c4817c60
コーヒー <Nicky01@live.se>
2018-11-30 11:20:41 +0100

    Added on-click

--- timewalker/mod.js
+++ timewalker/mod.js
@@ -1,41 +1,71 @@
-if(!cc)
+if (!cc)
 	throw "No Modloader Found!";
 
 document.body.addEventListener('modsLoaded', function () {
-	var timewalker = new function(){
+	var timewalker = new function () {
 		var proxies = undefined;
-		
+
 		ig.input.bind(84, "slowTime");
 		ig.input.bind(82, "resetTime");
 		ig.input.bind(67, "stopTime");
 		
-		function onUpdate(){
+		clicked = {
+			slowTime: false,
+			stopTime: false
+		}
+		held = {
+			slowTime: false,
+			stopTime: false
+		}
+
+		function onUpdate() {
 			var player = cc.ig.playerInstance();
 			var currentProxies;
-			
-			if(player !== null && 
-					(currentProxies = simplify.getEntityProxies(player)) !== proxies){
+
+			if (player !== null &&
+				(currentProxies = simplify.getEntityProxies(player)) !== proxies) {
 				modifyProxies(currentProxies, 0.2);
 			}
 			
-			
-			if(ig.input.state("slowTime") || ig.input.state("resetTime") || ig.input.state("stopTime"))
-				new cc.ig.combatActions.REMOVE_PROXIES({sticking: false, group:"aura"}).start(cc.ig.playerInstance());
-			
-			if(ig.input.state("slowTime") || ig.input.state("stopTime")) {
-				if (ig.input.state("slowTime"))
-					modifyProxies(currentProxies, 0.2)
-				else
-					modifyProxies(currentProxies, Math.pow(0.2, 10))
-				simplify.runAction(new cc.ig.combatActions.SHOOT_PROXY({ align: "BOTTOM", proxy:"infiniteSlowMo", offset:{x:0,y:0,z:0} }), cc.ig.playerInstance());
+			held.slowTime = ig.input.state("slowTime");
+			held.stopTime = ig.input.state("stopTime");
+
+			if (proxies !== undefined) {
+				if (ig.input.state("resetTime"))
+					new cc.ig.combatActions.REMOVE_PROXIES({sticking: false, group: "aura"}).start(cc.ig.playerInstance());
+
+				else if (!clicked.slowTime && held.slowTime) {
+					new cc.ig.combatActions.REMOVE_PROXIES({sticking: false, group: "aura"}).start(cc.ig.playerInstance());
+					modifyProxies(currentProxies, 0.2);
+					simplify.runAction(new cc.ig.combatActions.SHOOT_PROXY({
+						align: "BOTTOM",
+						proxy: "infiniteSlowMo",
+						offset: {x: 0, y: 0, z: 0}
+					}), cc.ig.playerInstance());
+				
+				} else if (!clicked.stopTime && held.stopTime) {
+					new cc.ig.combatActions.REMOVE_PROXIES({sticking: false, group: "aura"}).start(cc.ig.playerInstance());
+					modifyProxies(currentProxies, Math.pow(0.2, 10));
+					simplify.runAction(new cc.ig.combatActions.SHOOT_PROXY({
+						align: "BOTTOM",
+						proxy: "infiniteSlowMo",
+						offset: {x: 0, y: 0, z: 0}
+					}), cc.ig.playerInstance());
+				}
+				
+				if (clicked.slowTime != held.slowTime)
+					clicked.slowTime = held.slowTime;
+				
+				if (clicked.stopTime != held.stopTime)
+					clicked.stopTime = held.stopTime;
 			}
 		}
-		
-		function modifyProxies(currentProxies, time){
+
+		function modifyProxies(currentProxies, time) {
 			proxies = currentProxies;
 			proxies.infiniteSlowMo = proxies.lightningSlowMo;
 			var action = simplify.getProxyAction(proxies.infiniteSlowMo.data.action);
-			
+
 			action.group = "infinteSlowMo";
 			action = simplify.getNextProxyAction(action);
 			action.group = "infinteSlowMo";
@@ -44,7 +74,7 @@ document.body.addEventListener('modsLoaded', function () {
 			action = simplify.getNextProxyAction(action);
 			action.time = -1;
 		}
-		
+
 		simplify.registerUpdate(onUpdate);
 	}();
 });
